acd0ef3d20cf9c691bf40e3d6a0db61ea89e361e
Tales Pimentel <tales.tsp@gmail.com>
2018-07-29 10:46:56 -0300

    Adding selection WHERE to queries

--- dao/dao.py
+++ dao/dao.py
@@ -21,12 +21,12 @@ class DAO:
             DAO.instance.credentials_jon = credentials_jon
 
     def records_df(self, columns=["db_key", "userid", "tz", "time", "type"]):
-        records_df = pd.DataFrame(self.records(", ".join(columns)))
+        records_df = pd.DataFrame(self.records(select_columns=", ".join(columns)))
         records_df.columns = columns
         return records_df
 
-    def users_df(self, columns=["userid", "phonenumber", "test_user"]):
-        users_df = pd.DataFrame(self.users(", ".join(columns)))
+    def users_df(self, userids=[], columns=["userid", "phonenumber", "test_user"]):
+        users_df = pd.DataFrame(self.users(userids=userids, select_columns=", ".join(columns)))
         users_df.columns = columns
         return users_df
 
@@ -34,9 +34,16 @@ class DAO:
         query = "SELECT %s FROM records;" % (select_columns)
         return self.sql_query(query, verbose=True)
 
-    def users(self, select_columns="*"):
+    def users(self, userids=[], select_columns="*"):
         query = "SELECT %s FROM users;" % (select_columns)
 
+        if len(userids) > 0:
+            query = self.selection_match(query = query,
+                                         attribute="userid",
+                                         values=userids,
+                                         logical_conjunction="OR")
+
+        print(query)
         return self.sql_query(query, verbose=True)
 
     def sql_query(self, sql_query, verbose=False):
@@ -54,6 +61,17 @@ class DAO:
 
         return data
 
+    def selection_match(self, query, attribute, values, logical_conjunction):
+        appendix = " WHERE "
+
+        for value in values:
+            appendix = appendix + attribute + "=" + value + " " + logical_conjunction + " "
+
+        appendix = appendix[0:len(appendix) - len(logical_conjunction + " ")]
+
+        return query.replace(";", "") + appendix + ";"
+
+
     def __del__(self):
         self.instance.cnx.close()
 
@@ -61,7 +79,7 @@ class DAO:
 
 if __name__ == "__main__":
     dao = DAO(utils.credentials_db())
-    dao.users()
-
+    data = dao.users(userids=["6199", "6214", "6272"])
+    print(data)
 
 
