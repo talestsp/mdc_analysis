38af0fe3068eb564515c67a7d02c8469f789afe0
Tales Pimentel <tales.tsp@gmail.com>
2018-07-31 16:13:10 -0300

    Changing path for credentials due to structure change

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,7 +1,7 @@
 import json
 
 def credentials_db():
-    with open('credentials/credentials_mdc_mysql.json') as f:
+    with open('src/credentials/credentials_mdc_mysql.json') as f:
         credentials_db_json = json.load(f)
 
     return credentials_db_json
