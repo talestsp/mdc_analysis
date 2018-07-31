69f43d8feabe74a28910b766e40122d1a259d74c
Tales Pimentel <tales.tsp@gmail.com>
2018-07-31 16:15:04 -0300

    RecordTypes enum

--- src/data_processment/summary.py
+++ src/data_processment/summary.py
@@ -1,7 +1,8 @@
 from src.dao import dbdao
+from src.entity.record_types import RecordType
 import pandas as pd
 
-def user_records(save_to_filepath="outputs/user_records.csv"):
+def user_total_records(save_to_filepath="outputs/user_total_records.csv"):
     my_dao = dbdao.DBDAO()
     user_df = my_dao.users_df()
 
@@ -24,4 +25,15 @@ def user_records(save_to_filepath="outputs/user_records.csv"):
         print("")
 
 
-user_records()
+def user_gps_records(save_to_filepath="outputs/user_gps_records.csv"):
+    my_dao = dbdao.DBDAO()
+    user_df = my_dao.users_df()
+
+    user_records_list = []
+    use_userids = user_df["userid"]
+
+    for userid in use_userids:
+        records_df = my_dao.records_df(userids=[userid], record_type=RecordType.GPS.value)
+        print(records_df)
+
+user_gps_records()
