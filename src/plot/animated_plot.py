677f31131780aac0d488fcfcaf2ef6c148539bf1
Tales Pimentel <tales.tsp@gmail.com>
2018-08-16 18:01:34 -0300

    Animated plots done... still parametrization

--- src/plot/animated_plot.py
+++ src/plot/animated_plot.py
@@ -1,4 +1,5 @@
 from src.plot.stop_region_plot import plot_stop_region, plot_user_loc
+from src.utils.geo import cluster_centroid
 
 class AnimatedPlot:
 
@@ -9,13 +10,18 @@ class AnimatedPlot:
 
         self.indexed_clusters = {}
 
-        for cluster in clusters:
-            cluster = cluster.sort_values(by="time")
-            self.indexed_clusters[cluster.tail(1).index.name] = cluster
+        if clusters:
+            for cluster in clusters:
+                cluster = cluster.sort_values(by="time")
+                self.indexed_clusters[int(cluster.tail(1)["time"])] = cluster
 
 
     def build_base_plot(self, color="navy", alpha=0.2):
         return plot_user_loc(self.user_data, self.title, color=color, alpha=alpha)
+
+    def build_stop_region_plot(self, color="navy", circle_alpha=0.2, cluster_alpha=0.2):
+        return plot_stop_region(self.user_data, [self.indexed_clusters[key] for key in self.indexed_clusters.keys()],
+                                self.title, color=color, circle_alpha=circle_alpha, cluster_alpha=cluster_alpha)
 
     def get_next_point(self, reset=False):
         if reset:
@@ -24,7 +30,11 @@ class AnimatedPlot:
         next_point = self.user_data.iloc[self.next_point_i]
         self.next_point_i += 1
 
-        if next_point.name in self.indexed_clusters:
-            return {"point": next_point, "cluster": self.indexed_clusters[next_point.name]}
-        else:
-            return {"point": next_point}
+        for cluster_timestamp in self.indexed_clusters.keys():
+            if next_point["time"] >= cluster_timestamp:
+                cluster_found = self.indexed_clusters[cluster_timestamp]
+                del self.indexed_clusters[cluster_timestamp]
+
+                return {"point": next_point, "centroid": cluster_centroid(cluster_found)}
+
+        return {"point": next_point, "centroid": None}
--- src/plot/server.py
+++ src/plot/server.py
@@ -11,46 +11,72 @@ from src.dao.csv_dao import load_user_gps_csv
 from src.data_processment.stop_region import MovingCentroidStopRegionFinder
 from src.plot.animated_plot import AnimatedPlot
 
-user_data = load_user_gps_csv(6171, 0, 1)
-clusters = MovingCentroidStopRegionFinder(region_radius=50, delta_time=300).find_clusters(user_data, verbose=True)
-
-aplot = AnimatedPlot(user_data, clusters, "Animated Plot")
-p = aplot.build_base_plot(color="orange", alpha=0.5)
-
-head_circle = p.circle(x=[], y=[], line_color=[], size=[])
-
-ds = head_circle.data_source
 
 def callback():
-    period = 0.005
+    point_circle = p.circle(x=[], y=[], line_color=[], size=[])
+    ds_point = point_circle.data_source
+
+    centroid_circle = p.circle(x=[], y=[], size=[], fill_alpha=[], line_alpha=[], line_color=[], line_width=[])
+    ds_centroid = centroid_circle.data_source
+
+    period = 0.01
     last_time = time.time() + period
     counter = 0
 
-    sizes = []
-    new_data = dict()
+    point_sizes = []
+    new_point_data = dict()
+    new_centroid_data = dict()
 
     while counter < len(user_data):
         if time.time() - last_time >= period:
             last_time = time.time()
             counter += 1
 
-            point = aplot.get_next_point()["point"]
+            next = aplot.get_next_point()
 
-            new_data['x'] = ds.data["x"] + [point["longitude"]]
-            new_data['y'] = ds.data["y"] + [point["latitude"]]
-            new_data['line_color'] = ds.data["line_color"] + ["navy"]
-            new_data['size'] = sizes + [4]
+            point = next["point"]
+            centroid = next["centroid"]
 
-            sizes.append(2)
+            new_point_data['x'] = ds_point.data["x"] + [point["longitude"]]
+            new_point_data['y'] = ds_point.data["y"] + [point["latitude"]]
+            new_point_data['line_color'] = ds_point.data["line_color"] + ["navy"]
+            new_point_data['size'] = point_sizes + [4]
 
-            ds.data = new_data
+            point_sizes.append(2)
+
+            if not centroid is None:
+                new_centroid_data["x"] = ds_centroid.data["x"] + [centroid["longitude"]]
+                new_centroid_data["y"] = ds_centroid.data["y"] + [centroid["latitude"]]
+                new_centroid_data["size"] = ds_centroid.data["size"] + [20]
+                new_centroid_data["fill_alpha"] = ds_centroid.data["fill_alpha"] + [0.4]
+                new_centroid_data["line_alpha"] = ds_centroid.data["line_alpha"] + [0.3]
+                new_centroid_data["line_color"] = ds_centroid.data["line_color"] + ["firebrick"]
+                new_centroid_data["line_width"] = ds_centroid.data["line_width"] + [1]
+
+                ds_centroid.data = new_centroid_data
+
+            ds_point.data = new_point_data
 
 
+userid = 6171
+r = 50
+delta_t = 300
+from_day_n = 0
+n_days = 1
+
+print("LOADING USER DATA")
+user_data = load_user_gps_csv(userid, from_day_n, n_days)
+print("FINDING STOP REGIONS")
+clusters = MovingCentroidStopRegionFinder(region_radius=r, delta_time=delta_t).find_clusters(user_data, verbose=False)
+print("PLOTTING")
+
+aplot = AnimatedPlot(user_data, clusters, title="USERID: " + str(userid) + " - n_CLUSTERS: " + str(len(clusters)) + " - " + "d: " + str(r) + ", " + "delta_t: " + str(delta_t))
+p = aplot.build_stop_region_plot(color="orange", cluster_alpha=0.05)
 
 # add a button widget and configure with the call back
-button = Button(label="Go")
-button.on_click(callback)
+button_go = Button(label="Go")
+button_go.on_click(callback)
 
 # put the button and plot in a layout and add to the document
-curdoc().add_root(column(p, button))
+curdoc().add_root(column(p, button_go))
 
