ef0bc7e92cfd52ce17a82c64856c25a738a598c9
Tales Pimentel <tales.tsp@gmail.com>
2019-05-22 17:54:18 -0300

    Time diff

--- src/utils/time_utils.py
+++ src/utils/time_utils.py
@@ -1,6 +1,8 @@
 import datetime
 import calendar
 
+DAY_SECONDS = 86400
+
 def enrich_time_columns(gps_data, time_col="local_time"):
     gps_data["hour"] = [datetime.datetime.utcfromtimestamp(gps_time).hour for gps_time in gps_data[time_col]]
     gps_data["min"] = [datetime.datetime.utcfromtimestamp(gps_time).minute for gps_time in gps_data[time_col]]
@@ -16,3 +18,9 @@ def enrich_time_columns(gps_data, time_col="local_time"):
 def local_time(data, time_col="time", tz_col="tz"):
     data["local_" + time_col] = data[time_col] + data[tz_col]
     return data
+
+def diff_between_dates(timestamp0, timestamp1):
+    d0 = datetime.datetime.fromtimestamp(timestamp0)
+    d1 = datetime.datetime.fromtimestamp(timestamp1)
+    delta_days = (d1 - d0).days + ((d1 - d0).seconds / (DAY_SECONDS))
+    return delta_days
